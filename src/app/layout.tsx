34ecb262fb81049d12aca83640dcb7661150621d
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-12 13:30:13 +0900

    setting: Configure react-query provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import QueryProvider from '@/components/business/QueryProvider';
 
 import '@/styles/globals.css';
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
