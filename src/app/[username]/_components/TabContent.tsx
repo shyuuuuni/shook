fcbb8fa64e8969351856c9bd11f8ce7a140a7170
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-19 02:34:32 +0900

    feat: Add Repository overview tab contents

--- src/app/[username]/_components/TabContent.tsx
+++ src/app/[username]/_components/TabContent.tsx
@@ -1,9 +1,24 @@
 'use client';
 
+import * as ReactTabs from '@radix-ui/react-tabs';
+import { match } from 'ts-pattern';
 import useTab from '../_hooks/useTab';
+import RepositoryOverview from './RepositoryOverview';
 
 export default function TabContent() {
-  const { tab } = useTab();
+  const tab = useTab();
 
-  return <div>{tab}</div>;
+  return match(tab)
+    .with('repository', () => (
+      <ReactTabs.TabsContent value={'repository'}>
+        <RepositoryOverview />
+      </ReactTabs.TabsContent>
+    ))
+    .with('readme', () => (
+      <ReactTabs.TabsContent value={'readme'}>readme</ReactTabs.TabsContent>
+    ))
+    .with('activity', () => (
+      <ReactTabs.TabsContent value={'activity'}>activity</ReactTabs.TabsContent>
+    ))
+    .exhaustive();
 }
