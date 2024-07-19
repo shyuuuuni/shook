f0f63918b1b13cc98f9fb7d5fd9fb905e3b6a692
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-19 14:55:40 +0900

    style: Style profile section

--- src/app/[username]/_components/profile/ProfileSection.tsx
+++ src/app/[username]/_components/profile/ProfileSection.tsx
@@ -1,5 +1,5 @@
-import * as Avatar from '@radix-ui/react-avatar';
 import gitHubService from '@/backend/services/GitHubService';
+import UserProfile from './UserProfile';
 
 type ProfileSectionProps = {
   username: string;
@@ -11,18 +11,10 @@ export default async function ProfileSection({
   className,
 }: ProfileSectionProps) {
   const user = await gitHubService.getUserByUsername({ username });
-  const { followers, bio, name, email, created_at } = user;
 
   return (
     <section className={className}>
-      <Avatar.Root>
-        <Avatar.Image src={user.avatar_url} alt={`${username} 프로필 이미지`} />
-      </Avatar.Root>
-      <div>followers: {followers}</div>
-      <div>bio: {bio}</div>
-      <div>name: {name}</div>
-      <div>email: {email}</div>
-      <div>since: {created_at}</div>
+      <UserProfile user={user} />
     </section>
   );
 }
