69f8f619a4c6dddfde1db0dcfd6e9f4ca947541f
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-22 17:30:42 +0900

    feat: Add queries for achievements metrics

---
+++ src/libs/metrics/queries/index.ts
@@ -0,0 +1,4 @@
+export { default as QUERY_ACHIEVEMENTS_DEFAULT } from './achievements/achievements.graphql';
+export { default as QUERY_ACHIEVEMENTS_ORGANIZATIONS } from './achievements/organizations.graphql';
+export { default as QUERY_ACHIEVEMENTS_RANKING } from './achievements/ranking.graphql';
+export { default as QUERY_ACHIEVEMENTS_TOTAL } from './achievements/total.graphql';
