bb4be782b6352741d0882878dcb1e4199a71b6e9
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-23 16:20:23 +0900

    feat: Add access token to session for github api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,18 @@ export const handler = NextAuth({
       clientSecret: process.env.GITHUB_OAUTH_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
