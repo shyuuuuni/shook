266247ca19063c2b4ed84bd3b1289035f31806d6
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-23 18:03:23 +0900

    refactor: Separate next-auth configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-
-export const authOptions: AuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_OAUTH_CLIENT_ID,
-      clientSecret: process.env.GITHUB_OAUTH_SECRET,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/libs/next-auth/authOptions';
 
 const handler = NextAuth(authOptions);
 
