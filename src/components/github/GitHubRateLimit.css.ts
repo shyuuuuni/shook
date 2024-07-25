b24458325acc161ac88b7087a11b3d8e83fe569b
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-25 18:50:53 +0900

    style: Change opacity for GitHubRateLimit component

--- src/components/github/GitHubRateLimit.css.ts
+++ src/components/github/GitHubRateLimit.css.ts
@@ -6,7 +6,8 @@ export const container = style({
   right: 20,
   display: 'flex',
   flexDirection: 'column',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+  boxShadow: '0 5px 5px rgba(0, 0, 0, 0.2)',
   padding: '8px 16px',
   width: 'fit-content',
+  background: 'white',
 });
