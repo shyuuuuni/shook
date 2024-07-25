f517332b630298f9a6c028344891a2cdff22bb4f
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-25 23:18:28 +0900

    feat: Add API_URL environment variable for vercel

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,1 +1,7 @@
 export const APP_FLAG: 'TEST' | 'SERVICE' = 'TEST';
+
+const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.VERCEL_URL;
+
+export const API_URL = VERCEL_URL
+  ? `https://${VERCEL_URL}/api`
+  : 'http://localhost:3000/api';
