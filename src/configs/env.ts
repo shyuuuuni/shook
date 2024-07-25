ddbd7823263793313bd8624ff5f5eed2722a4127
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-25 22:47:22 +0900

    test

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,5 +2,5 @@ export const APP_FLAG: 'TEST' | 'SERVICE' = 'TEST';
 
 export const API_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}/api`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
     : 'http://localhost:3000/api';
