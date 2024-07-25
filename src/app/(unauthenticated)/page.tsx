2186b1ba07e64fbaad69b0a01c056d02ed6a0bac
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-25 22:43:17 +0900

    test2

--- src/app/(unauthenticated)/page.tsx
+++ src/app/(unauthenticated)/page.tsx
@@ -1,14 +1,13 @@
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth';
+import { API_URL } from '@/configs/env';
 import { authOptions } from '@/libs/next-auth';
 import GitHubLoginButton from './_components/GitHubLoginButton';
 import * as styles from './page.css';
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/test`,
-  );
+  const response = await fetch(`${API_URL}/test`);
   const data = await response.json();
 
   console.log(data);
--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,1 +1,6 @@
 export const APP_FLAG: 'TEST' | 'SERVICE' = 'TEST';
+
+export const API_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_ENV}/api`
+    : 'http://localhost:3000/api';
