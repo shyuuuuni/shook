22c077e23708d0f685f40157770b7ae29acf9430
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-26 12:11:40 +0900

    refactor: Update API_URL environment variable for Vercel

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,9 @@
-const VERCEL_URL = process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.VERCEL_URL;
+const VERCEL_URL =
+  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ??
+  process.env.VERCEL_PROJECT_PRODUCTION_URL;
 
 export const API_URL = VERCEL_URL
   ? `https://${VERCEL_URL}/api`
   : 'http://localhost:3000/api';
+
+console.log('Current API URL', API_URL, VERCEL_URL);
