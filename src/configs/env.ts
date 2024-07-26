f724e5e50f3579f2c981cf378b5c785bc8931940
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-07-26 14:38:48 +0900

    fix: Update API_URL to include "/api" path

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,4 +1,3 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_SERVER_URL ?? process.env.API_SERVER_URL;
+export const API_URL = `${process.env.NEXT_PUBLIC_API_SERVER_URL ?? process.env.API_SERVER_URL}/api`;
 
 console.log('API_URL', API_URL);
