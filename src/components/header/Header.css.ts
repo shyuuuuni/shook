1ffc5d3021de75a7234d9b6bb91837e014eaa2cb
Seung-hyun Kim <seunghyun4525@gmail.com>
2024-08-02 06:07:21 +0900

    style: Add header size & filter style

--- src/components/header/Header.css.ts
+++ src/components/header/Header.css.ts
@@ -1,9 +1,15 @@
 import { style } from '@vanilla-extract/css';
-import { HEADER_MAX_WIDTH } from '@/styles/constants';
+import { HEADER_HEIGHT, HEADER_MAX_WIDTH } from '@/styles/constants';
 
 export const header = style({
   display: 'flex',
   justifyContent: 'center',
+  height: HEADER_HEIGHT,
+  position: 'sticky',
+  top: '0',
+  backdropFilter: 'saturation(180%) blur(5px)',
+  background: 'hsla(0, 0%, 100%, .8)',
+  borderBottom: '1px solid hsla(0, 0%, 0%, .1)',
 });
 
 export const nav = style({
--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,3 +1,3 @@
-export const HEADER_MAX_WIDTH = 1440;
-export const CONTENT_MAX_WIDTH = 1280;
+export const HEADER_MAX_WIDTH = 1280;
+export const CONTENT_MAX_WIDTH = 1140;
 export const HEADER_HEIGHT = 64;
